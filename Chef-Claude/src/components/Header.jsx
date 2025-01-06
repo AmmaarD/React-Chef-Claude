944ec7d25af34dea8611b5b0eec2c0abd85cc8a8
Ammaar Doctor <ammaar.doctor@gmail.com>
2025-01-06 10:12:23 -0600

    completed header

---
+++ Chef-Claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import imgURL from "../images/Chef-Claude-Icon.png";
+
+export default function Header() {
+  return (
+      <header>
+          <img src={imgURL} />
+          <h1>Chef Claude</h1>
+      </header>
+  )
+}
