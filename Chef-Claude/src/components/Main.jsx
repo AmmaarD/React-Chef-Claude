9555880721f709093929eac2838c4f82a73b98fb
Ammaar Doctor <ammaar.doctor@gmail.com>
2025-01-11 14:38:19 -0600

    added state for adding ingredients

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -1,14 +1,34 @@
+import React, { useState } from "react"
+
 export default function Main () {
+  
+  const [ingredients, setIngredients] = React.useState([])
+    
+  const ingredientsListItems = ingredients.map(ingredient => (
+      <li key={ingredient}>{ingredient}</li>
+  ))
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prev => [...prev, newIngredient])
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input 
           type="text" 
           aria-label="Add ingredient"
           placeholder="e.g. oregano"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>
+        {ingredientsListItems}
+      </ul>
     </main>
   )
 }
